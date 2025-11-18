7b8acac4ccfbced632879c1467b0ea87aaf95e57
athallaabhi <athallaabhi@gmail.com>
2025-11-18 21:51:27 +0700

    Remove unnecessary script.js file
    - Deleted empty script.js file that served no purpose
    - Removed script.js reference from index.html

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// ===================================
-// Main Script File
-// Loads separate simulation modules
-// ===================================
-
-// Note: Pendulum simulation is loaded from pendulum.js
-// Note: GLBB simulation is loaded from glbb.js
-
